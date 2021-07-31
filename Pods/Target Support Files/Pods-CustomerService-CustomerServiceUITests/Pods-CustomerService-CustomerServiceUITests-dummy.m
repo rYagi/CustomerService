59967da22d7e6e1c539040bd881778d08fad8378
yagi <r.yagi@live.jp>
2021-08-01 00:17:46 +0900

    include scheme etc

---
+++ Pods/Target Support Files/Pods-CustomerService-CustomerServiceUITests/Pods-CustomerService-CustomerServiceUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CustomerService_CustomerServiceUITests : NSObject
+@end
+@implementation PodsDummy_Pods_CustomerService_CustomerServiceUITests
+@end
